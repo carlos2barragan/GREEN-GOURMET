c43d61f83256eb2145368018c95e7c5b63773ad8
carlos2barragan <bcarlosans804@gmail.com>
2024-08-10 19:46:11 -0500

    se agrego el final de javascript en todas las paginas

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
-const btn_darkmode = document.getElementById('darkmode');
+const btn_darkmode = document.getElementById("darkmode");
+ 
+document.addEventListener("DOMContentLoaded", e => {
+  cargarModooscuro()
+  btn_darkmode.addEventListener("click", toggleDarkMode) 
+})
 
-btn_darkmode.addEventListener('click', function(){
-   
-    document.body.classList.toggle('dark');
-    
-    
-});
+ function toggleDarkMode(){
+  btn_darkmode.classList.toggle("dark"); 
+  document.body.classList.toggle("dark");
+  guardarModoOscuro(btn_darkmode.classList.contains("dark"))
+ }
 
+
+ function guardarModoOscuro(estado){
+  localStorage.setItem("modoDark", estado)
+ }
+ function cargarModooscuro(){
+  const modoDarkguardado = localStorage.getItem("modoDark") ==="true";
+  if ( modoDarkguardado){
+    btn_darkmode.classList.add("dark"); 
+    document.body.classList.add("dark");
+  }
+ }
